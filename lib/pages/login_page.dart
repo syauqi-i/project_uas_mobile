0839329664dd077586f7eda6e50f7c43e26267e5
syauqi-i <amerikaserver21@gmail.com>
2026-01-15 07:12:36 +0700

    addedd new update

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,126 +1,213 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 import 'package:project_uas/pages/main_page.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  Widget build(BuildContext context) {
+    return const HomeUI();
+  }
 }
 
-class _LoginPageState extends State<LoginPage> {
-  // akun dummy
-  final String validEmail = "admin@gmail.com";
-  final String validPassword = "123456";
-  
+class HomeUI extends StatefulWidget {
+  const HomeUI({super.key});
 
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  @override
+  State<HomeUI> createState() => _HomeUIState();
+}
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class _HomeUIState extends State<HomeUI> {
+  // dummy account
+  final String validEmail = "syauqi@gmail.com";
+  final String validPassword = "12345";
 
-  void handleLogin() {
-    if (_formKey.currentState!.validate()) {
-      final email = emailController.text.trim();
-      final password = passwordController.text.trim();
+  FocusNode emailFocusNode = FocusNode();
+  TextEditingController emailController = TextEditingController();
 
-      if (email == validEmail && password == validPassword) {
-        // pindah halaman
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const MainPage()),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Email atau Password salah")),
-        );
-      }
-    }
+  FocusNode passwordFocusNode = FocusNode();
+  TextEditingController passwordController = TextEditingController();
+
+  StateMachineController? controller;
+
+  SMIBool? lookOnEmail;
+  SMINumber? followOnEmail;
+  SMIBool? lookOnPassword;
+  SMIBool? peekOnPassword;
+  SMITrigger? triggerSuccess;
+  SMITrigger? triggerFail;
+
+  @override
+  void initState() {
+    emailFocusNode.addListener(() {
+      lookOnEmail?.change(emailFocusNode.hasFocus);
+    });
+
+    passwordFocusNode.addListener(() {
+      lookOnPassword?.change(passwordFocusNode.hasFocus);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(24),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const Text(
-                  "LOGIN",
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-
-                SizedBox(height: 300, width: 300), // slot animasi (Rive nanti)
-                // EMAIL
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: "Email",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Email wajib diisi";
-                    }
-                    if (!value.contains("@")) {
-                      return "Format email tidak valid";
-                    }
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: 16),
-
-                // PASSWORD
-                TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: "Password",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Password wajib diisi";
-                    }
-                    if (value.length < 6) {
-                      return "Password minimal 6 karakter";
-                    }
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: 24),
-
-                // BUTTON LOGIN
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: handleLogin,
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 14),
-                    ),
-                    child: const Text("LOGIN", style: TextStyle(fontSize: 16)),
-                  ),
-                ),
-              ],
+      backgroundColor: const Color.fromARGB(255, 153, 18, 92),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            const SizedBox(height: 86),
+            Text(
+              "MoneyTrack",
+              textAlign: TextAlign.center,
+              style: Theme.of(
+                context,
+              ).textTheme.headlineMedium?.copyWith(color: Colors.white),
             ),
-          ),
+            SizedBox(height: 80),
+
+            /// RIVE
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: RiveAnimation.asset(
+                "assets/animation/auth-teddy.riv",
+                fit: BoxFit.fitHeight,
+                onInit: (artboard) {
+                  controller = StateMachineController.fromArtboard(
+                    artboard,
+                    "Login Machine",
+                  );
+
+                  if (controller == null) return;
+                  artboard.addController(controller!);
+
+                  lookOnEmail = controller?.getBoolInput("isFocus");
+                  followOnEmail = controller?.getNumberInput("numLook");
+                  lookOnPassword = controller?.getBoolInput("isPrivateField");
+                  peekOnPassword = controller?.getBoolInput(
+                    "isPrivateFieldShow",
+                  );
+                  triggerSuccess = controller?.getTriggerInput(
+                    "successTrigger",
+                  );
+                  triggerFail = controller?.getTriggerInput("failTrigger");
+                },
+              ),
+            ),
+
+            /// FORM
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    focusNode: emailFocusNode,
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      followOnEmail?.change(value.length * 1.5);
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: passwordController,
+                    focusNode: passwordFocusNode,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                    ),
+                    onTap: () {
+                      peekOnPassword?.change(true);
+                    },
+                  ),
+                  const SizedBox(height: 32),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 15, 9, 51),
+                      ),
+
+                      onPressed: onClickLogin,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white30,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void onClickLogin() async {
+    emailFocusNode.unfocus();
+    passwordFocusNode.unfocus();
+
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => const Center(child: CircularProgressIndicator()),
+    );
+
+    await Future.delayed(const Duration(seconds: 1));
+    if (!mounted) return;
+
+    Navigator.pop(context);
+
+    final valid =
+        email.toLowerCase() == validEmail.toLowerCase() &&
+        password == validPassword;
+
+    if (valid) {
+      //  trigger animasi sukses
+      triggerSuccess?.fire();
+
+      //  tunggu animasi selesai
+      await Future.delayed(const Duration(seconds: 2));
+
+      if (!mounted) return;
+
+      //  pindah ke MainPage
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => MainPage()),
+      );
+    } else {
+      triggerFail?.fire();
+    }
   }
 }
